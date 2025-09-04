225ac9b889df0cd9017096eabd49c0626a465a79
Mohit Bisht <100072596+bishtmohit872@users.noreply.github.com>
2025-09-04 15:43:34 +0530

    Update env.js

--- config/env.js
+++ config/env.js
@@ -11,6 +11,7 @@ export const { PORT,
     ARCJET_KEY,
     QSTASH_URL,
     QSTASH_TOKEN,
-    SERVER_URL,
+    SERVER_URL:"https://subscription-renewal-tracker.onrender.com",
+    // SERVER_URL,
     EMAIL_PASSWORD
 } = process.env
